e3861f5029c1c2df7a3e98711786c7ed9d47d1bd
LukaszK1983 <luke83@poczta.fm>
2020-05-06 15:13:01 +0200

    task 11

--- src/App.js
+++ src/App.js
@@ -2,6 +2,55 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Krok 11 przykład z filtrowanie listy
+
+const allUsers = ['Michal', 'Kasia', 'Jacek', 'Marta', 'Tomek', 'Ania'];
+
+class ListFilterApp extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            filteredUsers: allUsers
+        };
+    }
+
+    filterUsers(e) {
+        const text = e.currentTarget.value;
+        const filteredUsers = this.getFilteredUsersForText(text)
+        this.setState({
+            filteredUsers
+        })
+    }
+
+    getFilteredUsersForText(text) {
+        return allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()))
+    }
+
+    render () {
+        return (
+            <div>
+                <input onInput={this.filterUsers.bind(this)} />
+                <UsersList users={this.state.filteredUsers} />
+            </div>
+        );
+    }
+}
+
+const UsersList = ({ users }) => {
+    if (users.length > 0) {
+        return (
+            <ul>
+                {users.map(user => <li key={user}>{user}</li>)}
+            </ul>
+        );
+    }
+
+    return (
+        <p>Brak wyników!</p>
+    );
+};
+
 function App() {
   return (
     <div className="App">
@@ -20,7 +69,8 @@ function App() {
         </a>
       </header>
       <br/>
-
+      <p>Zadanie z punktu 11</p>
+      <ListFilterApp/>
     </div>
   );
 }
