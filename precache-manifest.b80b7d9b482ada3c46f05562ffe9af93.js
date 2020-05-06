49afbbc91770a2a1b4d46acf9ef218a5ea836c0b
LukaszK1983 <luke83@poczta.fm>
2020-05-06 15:24:49 +0200

    Updates

---
+++ precache-manifest.b80b7d9b482ada3c46f05562ffe9af93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ba778717c265bb8f55ba0c9c1336d0",
+    "url": "/testreact-app/index.html"
+  },
+  {
+    "revision": "dfcdc6c5dfa926823b36",
+    "url": "/testreact-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b5e1289a1b9c1d381191",
+    "url": "/testreact-app/static/js/2.abd7a797.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testreact-app/static/js/2.abd7a797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfcdc6c5dfa926823b36",
+    "url": "/testreact-app/static/js/main.d1297418.chunk.js"
+  },
+  {
+    "revision": "b6e552d88a2994e86ee3",
+    "url": "/testreact-app/static/js/runtime-main.2a274e28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testreact-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
